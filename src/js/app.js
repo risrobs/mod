e021cedd7c3056817e1d6f1a11e05d153e65f12b
risrobs <robertivanov22@abv.bg>
2021-12-18 13:12:20 +0200

    kop

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import "../scss/app.scss";
+import * as R from 'ramda'
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -9,4 +10,10 @@ window.addEventListener("DOMContentLoaded", () => {
     { job: "programmer", class: "is-danger" },
   ];
   const articles = document.querySelectorAll("article");
+
+  const classes = R.pluck("class", arrayToPluck)
+  
+    for (let i = 0; i < articles.length; i++) {
+      articles[i].classList.add(classes[i]) 
+    }
 });
